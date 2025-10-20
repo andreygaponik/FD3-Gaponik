d0d56890b3593b5658d499818ac4f276e492bca0
andreygaponik <andreygaponik@gmail.com>
2025-10-20 10:37:22 +0300

    first commit

---
+++ ishop0/App.js
@@ -0,0 +1,6 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Shop from './components/Shop';
+
+ReactDOM.render(<Shop name="Shop name" address="Address" />, document.getElementById('container'));
---
+++ ishop0/components/Shop.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+
+import './Shop.css';
+
+class Shop extends React.Component {
+  render() {
+    return (
+      <div className="shop">
+        <h1>{this.props.name}</h1>
+        <h2>{this.props.address}</h2>
+      </div>
+    );
+  }
+}
+
+export default Shop;
