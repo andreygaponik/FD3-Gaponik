aee0ad999b2868a1e6a548d64329dbe948157aa9
andreygaponik <andreygaponik@gmail.com>
2025-10-20 10:38:46 +0300

    test VS commit

--- ishop0/App.js
+++ ishop0/App.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Shop from './components/Shop';
 
-ReactDOM.render(<Shop name="Shop name" address="Address" />, document.getElementById('container'));
+ReactDOM.render(
+  <Shop name="Shop name 1" address="Address" />,
+  document.getElementById('container'),
+);
